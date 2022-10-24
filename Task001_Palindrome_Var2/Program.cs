d51a6ccacb442ca73b887661727d5302bf1a7922
KharitonovaElena <lenuciya@mail.ru>
2022-10-24 18:17:59 +0300

    Find palindrome var2

---
+++ Task001_Palindrome_Var2/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом. 
+// Без использования строк
+
+// 14212 -> нет
+
+// 12821 -> да
+
+// 23432 -> да
+int Prompt(string msg)
+{
+     System.Console.Write(msg);
+     int number = Convert.ToInt32(Console.ReadLine());
+     return number;
+}
+int number = Prompt("Введите натуральное число: ");
+int temp = number;
+int result = 0;
+while (temp > 0)
+{
+int remain = temp % 10;
+result = result * 10 + remain;
+temp = temp / 10;
+}
+if (result == number)
+{
+     System.Console.WriteLine($"Число {number} является палиндромом");
+}
+else
+{
+     System.Console.WriteLine($"Число {number}  не является палиндромом");
+} 
