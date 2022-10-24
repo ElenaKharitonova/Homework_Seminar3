6e434ec0e15769c72f74c1e5baa1a50c3506336c
KharitonovaElena <lenuciya@mail.ru>
2022-10-24 13:15:33 +0300

    Check palindrome

---
+++ Task001_Palindrome/Program.cs
@@ -0,0 +1,36 @@
+﻿int Prompt(string msg)
+{
+     System.Console.Write(msg);
+     int number = Convert.ToInt32(Console.ReadLine());
+     return number;
+}
+bool ValidateNumber(int number)
+{
+     if (number < 10000 || number >= 100000)
+     {
+          System.Console.WriteLine("Это число не пятизначное");
+          return false;
+     }
+     return true;
+}
+
+int number = Prompt("Введите пятизначное натуральное число: ");
+if (ValidateNumber(number))
+{
+     int a = number / 10000;
+     int tempb = number / 1000;
+     int b = tempb % 10;
+     int tempc = number / 100;
+     int c = tempc % 10;
+     int tempd = number / 10;
+     int d = tempd % 10;
+     int e = number / 10000;
+     if (a == e && b == d)
+     {
+          System.Console.WriteLine($"Число {number} является палиндромом");
+     }
+     else
+     {
+          System.Console.WriteLine($"Число {number}  не является палиндромом");
+     } 
+}
